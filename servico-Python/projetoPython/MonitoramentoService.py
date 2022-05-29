07eb659a2531b0ae6dca2fbf1e351633be74d35d
FelipeDetoni <felipedepicolidetoni@gmail.com>
2022-05-29 19:03:55 -0300

    main
    from ctypes.wintypes import PINT
    import subprocess
    import json
    import requests
    from RaspMonitoramento import RaspMonitoramento
    from MonitoramentoUtil import MonitoramentoUtil
    import schedule
    import time
    
    def req():
    #Montamos a requisição para enviarmos o put para o webservice
        URL = "http://localhost:8080/webserviceMonitoramento/service/ws/info/post"
        headers={'Content-type':'application/json'}
        cont = 0
        while cont < 10:
            raspMon = MonitoramentoUtil.executeCommandPreencheObject()
            jsonRequest = raspMon.toJSON()
            cont = cont + 1
            request = requests.put(URL, json=jsonRequest, headers=headers)
            print(request.content)
    
        schedule.every(1).minutes.do(req)
    
        while True:
            schedule.run_pending()
            time.sleep(1)

--- servico-Python/projetoPython/MonitoramentoService.py
+++ servico-Python/projetoPython/MonitoramentoService.py
@@ -4,16 +4,23 @@ import json
 import requests
 from RaspMonitoramento import RaspMonitoramento 
 from MonitoramentoUtil import MonitoramentoUtil 
+import schedule
+import time
 
+def req():
 #Montamos a requisição para enviarmos o put para o webservice
-URL = "http://localhost:8080/webserviceMonitoramento/service/ws/info/post"
-headers={'Content-type':'application/json'}
-cont = 0
-while cont < 10:
-    raspMon = MonitoramentoUtil.executeCommandPreencheObject()
-    jsonRequest = raspMon.toJSON()
-    cont = cont + 1
-    request = requests.put(URL, json=jsonRequest, headers=headers)
-    print(request.content)
+    URL = "http://localhost:8080/webserviceMonitoramento/service/ws/info/post"
+    headers={'Content-type':'application/json'}
+    cont = 0
+    while cont < 10:
+        raspMon = MonitoramentoUtil.executeCommandPreencheObject()
+        jsonRequest = raspMon.toJSON()
+        cont = cont + 1
+        request = requests.put(URL, json=jsonRequest, headers=headers)
+        print(request.content)
+    
+    schedule.every(1).minutes.do(req)
 
-
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
--- servico-Python/projetoPython/MonitoramentoUtil.py
+++ servico-Python/projetoPython/MonitoramentoUtil.py
@@ -7,15 +7,15 @@ class MonitoramentoUtil:
     @staticmethod
     def executeCommandPreencheObject():
         #Comandos a serem executados no cmd 
-        cmdCpuDisp = ['wmic', 'memorychip', 'get', 'speed']
-        cmdCpuDispUtilizada = ['wmic', 'memorychip', 'get', 'speed']
-        cmdRamDisp = ['wmic', 'memorychip', 'get', 'speed']
-        cmdRamUtilizada = ['wmic', 'memorychip', 'get', 'speed']
-        cmdDiscoDisp =  ['wmic', 'memorychip', 'get', 'speed']
-        cmdDiscoUtilizado = ['wmic', 'memorychip', 'get', 'speed']
-        cmdTemperatura =  ['wmic', 'memorychip', 'get', 'speed']
-        cmdQntdProcessos = ['wmic', 'memorychip', 'get', 'speed']
-        cmdConRede = ['wmic', 'memorychip', 'get', 'speed']
+        cmdCpuDisp = ['dstat', '-c', '1', '1']
+        cmdCpuDispUtilizada = ['dstat', '-c', '1', '1']
+        cmdRamDisp = ['dstat', '-m', '1', '1']
+        cmdRamUtilizada = ['dstat', '-m', '1', '1']
+        cmdDiscoDisp =  ['dstat', '-d', '1', '1']
+        cmdDiscoUtilizado = ['dstat', '-d', '1', '1']
+        cmdTemperatura =  ['dstat', '-c', '1', '1']
+        cmdQntdProcessos = ['dstat', '-c', '1', '1']
+        cmdConRede = ['ifconfig']
         codRasp = 1 
 
         #Informações recuperadas do cmd após execução dos comandos
@@ -34,14 +34,14 @@ class MonitoramentoUtil:
         raspMon = RaspMonitoramento(monInfoCpuDisp, monInfoCpuUtilizada, monInfoRamDisp, monInfoRamUtilizada, monInfoDiscoDisp, monInfoDiscoUtilizado, monInfoTemperatura, monInfoQntdProcessos, monInfoConRede, monCodRasp)
 
         #Recuperamos apenas o primeiro número recuperado do cmd, que diz respeito a informação solicitada
-        raspMon.monInfoCpuDisp = raspMon.removerCaracteresEspeciais(raspMon.monInfoCpuDisp)[1]
-        raspMon.monInfoCpuUtilizada = raspMon.removerCaracteresEspeciais(raspMon.monInfoCpuUtilizada)[1]
-        raspMon.monInfoRamDisp = raspMon.removerCaracteresEspeciais(raspMon.monInfoRamDisp)[1]
-        raspMon.monInfoRamUtilizada = raspMon.removerCaracteresEspeciais(raspMon.monInfoRamUtilizada)[1]
-        raspMon.monInfoDiscoDisp = raspMon.removerCaracteresEspeciais(raspMon.monInfoDiscoDisp)[1]
-        raspMon.monInfoDiscoUtilizado = raspMon.removerCaracteresEspeciais(raspMon.monInfoDiscoUtilizado)[1]
-        raspMon.monInfoTemperatura = raspMon.removerCaracteresEspeciais(raspMon.monInfoTemperatura)[1]
-        raspMon.monInfoQntdProcessos = raspMon.removerCaracteresEspeciais(raspMon.monInfoQntdProcessos)[1]
+        raspMon.monInfoCpuDisp = raspMon.removerCaracteresEspeciais(raspMon.monInfoCpuDisp)[0]
+        raspMon.monInfoCpuUtilizada = raspMon.removerCaracteresEspeciais(raspMon.monInfoCpuUtilizada)[0]
+        raspMon.monInfoRamDisp = raspMon.removerCaracteresEspeciais(raspMon.monInfoRamDisp)[0]
+        raspMon.monInfoRamUtilizada = raspMon.removerCaracteresEspeciais(raspMon.monInfoRamUtilizada)[0]
+        raspMon.monInfoDiscoDisp = raspMon.removerCaracteresEspeciais(raspMon.monInfoDiscoDisp)[0]
+        raspMon.monInfoDiscoUtilizado = raspMon.removerCaracteresEspeciais(raspMon.monInfoDiscoUtilizado)[0]
+        raspMon.monInfoTemperatura = raspMon.removerCaracteresEspeciais(raspMon.monInfoTemperatura)[0]
+        raspMon.monInfoQntdProcessos = raspMon.removerCaracteresEspeciais(raspMon.monInfoQntdProcessos)[0]
 
         return raspMon
 
